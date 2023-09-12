b4424f5b32eb2b0381392bb687b64b0a3f5aa352
aBoelghet <aboelghet.ios.dev@gmail.com>
2023-09-12 09:57:24 +0300

    matrix of images

--- iOS SwiftUI/ContentView.swift
+++ iOS SwiftUI/ContentView.swift
@@ -8,16 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let matrix: [[String]] = [
+        ["lemon", "lemon", "lemon"],
+        ["lemon", "lemon", "lemon"],
+        ["lemon", "lemon", "lemon"]
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ForEach(0..<matrix.count, id: \.self) { row in
+                HStack {
+                    ForEach(0..<matrix[row].count, id: \.self) { col in
+                        Image(matrix[row][col])
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 120, height: 120)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
