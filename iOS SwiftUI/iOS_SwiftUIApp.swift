d807b9339e98230e65f4df0494aa2d54647fc499
aBoelghet <aboelghet.ios.dev@gmail.com>
2023-09-12 09:25:43 +0300

    Initial Commit

---
+++ iOS SwiftUI/iOS_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_SwiftUIApp.swift
+//  iOS SwiftUI
+//
+//  Created by aBoelghet  on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
